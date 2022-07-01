073d4c804a481225de55f952a3d3cd4f6b1ba737
Myrrdin <stephan.jeanba@gmail.com>
2022-07-01 12:08:23 +0200

    [✨ feat](config): mise en place d'un routing basic
    Routing basic basé sur le fichier htacess
    
    BREAKING CHANGE:
    Si le fichier htaccess la réécriture de l'url ne fonctionnera plus exposant les paramètres du
    routeur

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+
+define("URL", str_replace("index.php", "", (isset($_SERVER['HTTPS']) ? "https" : "http") .
+    "://" . $_SERVER['HTTP_HOST'] . $_SERVER["PHP_SELF"]));
+
+try {
+    if (empty($_GET['page'])) {
+        $page = "accueil";
+    } else {
+        $url = explode("/", filter_var($_GET['page'], FILTER_SANITIZE_URL));
+        $page = $url[0];
+    }
+
+    switch ($page) {
+        case "accueil":
+            // $mainController->accueil();
+            print_r("accueil");
+            break;
+
+            break;
+        default:
+            throw new Exception("La page n'existe pas");
+    }
+} catch (Exception $e) {
+    print_r($e->getMessage());
+}
