772ffde8b604a279efcd56be02ef657cbbe38647
Myrrdin <stephan.jeanba@gmail.com>
2022-07-05 00:55:39 +0200

    [✨ feat](Model): création des méthode de verification et ajout à la bdd
    Verification de la disponibilité du pseudo et du mail
    Insertion des information dans la BDD

--- models/Visitor/Visitor.model.php
+++ models/Visitor/Visitor.model.php
@@ -12,4 +12,36 @@ class VisitorManager extends MainManager
         $req->closeCursor();
         return $datas;
     }
+    public function isPseudoAvailable($pseudo): bool
+    {
+        $req = $this->getBdd()->prepare('SELECT * FROM user WHERE username = :pseudo');
+        $req->bindValue(':pseudo', $pseudo, PDO::PARAM_STR);
+        $req->execute();
+        $available = ($req->rowCount() === 0);
+        $req->closeCursor();
+        return $available;
+    }
+
+    public function isMailAvailable($mail): bool
+    {
+        $req = $this->getBdd()->prepare('SELECT * FROM user WHERE email = :mail');
+        $req->bindValue(':mail', $mail, PDO::PARAM_STR);
+        $req->execute();
+        $available = ($req->rowCount() === 0);
+        $req->closeCursor();
+        return $available;
+    }
+
+    public  function createNewAccount($password, $pseudo, $mail, $key)
+    {
+        $req = $this->getBdd()->prepare('INSERT INTO user ( username,email,password,create_time,role,isValid,linkValid) VALUES (:pseudo,:mail,:password,current_timestamp(),0,0,:key)');
+        $req->bindValue(':password', $password, PDO::PARAM_STR);
+        $req->bindValue(':mail', $mail, PDO::PARAM_STR);
+        $req->bindValue(':pseudo', $pseudo, PDO::PARAM_STR);
+        $req->bindValue(':key', $key, PDO::PARAM_INT);
+        $req->execute();
+        $isAdded = ($req->rowCount() > 0);
+        $req->closeCursor();
+        return $isAdded;
+    }
 }
