d53bf33d43f2478e34837b54e48399bfe48b35e4
Myrrdin <stephan.jeanba@gmail.com>
2022-07-06 00:11:21 +0200

    [💚 chores](Experiment): test de la fonction mail
    !!! Config Xampp !!!!

--- views/accueil.view.php
+++ views/accueil.view.php
@@ -4,4 +4,10 @@ require_once("components/grid.php");
 require_once("components/jumbotron.php");
 print_r($page_data);
 $articles = $page_data;
+$headers = "From: seror67@gmail.com";
+$sendTo = "stephan.jeanba@gmail.com";
+$subject = "test subject";
+$content = "test content";
+
+mail($sendTo, $subject, $content, $headers);
 require_once("components/articlesListe.php");
