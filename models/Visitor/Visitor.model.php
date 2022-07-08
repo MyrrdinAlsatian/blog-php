ddd82c9d2b31bb8f99e092c57f227e5307c050da
Myrrdin <stephan.jeanba@gmail.com>
2022-07-08 14:52:45 +0200

    [♻️ refactor]: ajout dynamique du role avec par défaut 0

--- models/Visitor/Visitor.model.php
+++ models/Visitor/Visitor.model.php
@@ -32,13 +32,14 @@ class VisitorManager extends MainManager
         return $available;
     }
 
-    public  function createNewAccount($password, $pseudo, $mail, $key)
+    public  function createNewAccount($password, $pseudo, $mail, $key, $role = 0)
     {
-        $req = $this->getBdd()->prepare('INSERT INTO user ( username,email,password,create_time,role,isValid,linkValid) VALUES (:pseudo,:mail,:password,current_timestamp(),0,0,:key)');
+        $req = $this->getBdd()->prepare('INSERT INTO user ( username,email,password,create_time,role,isValid,linkValid) VALUES (:pseudo,:mail,:password,current_timestamp(),:role,0,:key)');
         $req->bindValue(':password', $password, PDO::PARAM_STR);
         $req->bindValue(':mail', $mail, PDO::PARAM_STR);
         $req->bindValue(':pseudo', $pseudo, PDO::PARAM_STR);
         $req->bindValue(':key', $key, PDO::PARAM_INT);
+        $req->bindValue(':role', $role, PDO::PARAM_INT);
         $req->execute();
         $isAdded = ($req->rowCount() > 0);
         $req->closeCursor();
