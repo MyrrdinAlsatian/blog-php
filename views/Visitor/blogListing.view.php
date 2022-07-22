79382bce7acc128b708e179fa8a962e890990f6f
Myrrdin <stephan.jeanba@gmail.com>
2022-07-22 16:54:43 +0200

    [🐛 fix]: rajout des liens veres les articles

--- views/Visitor/blogListing.view.php
+++ views/Visitor/blogListing.view.php
@@ -6,13 +6,15 @@
             foreach ($articles as $article) :
             ?>
                 <li class="blogs-bloc__item">
-                    <article class="blogs-bloc__item-article">
-                        <time class="blogs-bloc__item-tag" datetime=""> <?= $article["readingTime"] ?> minutes </time>
-                        <img class="blogs-bloc__item-img" src="<?= $article['featureImage'] ?>" alt="blog" />
-                        <header>
-                            <h3 class="blogs-bloc__item-header"><?= $article["title"]; ?></h3>
-                        </header>
-                    </article>
+                    <a href="<?= URL . 'article/' . $article['id'] ?>">
+                        <article class="blogs-bloc__item-article">
+                            <time class="blogs-bloc__item-tag" datetime=""> <?= $article["readingTime"] ?> minutes </time>
+                            <img class="blogs-bloc__item-img" src="<?= $article['featureImage'] ?>" alt="blog" />
+                            <header>
+                                <h3 class="blogs-bloc__item-header"><?= $article["title"]; ?></h3>
+                            </header>
+                        </article>
+                    </a>
                 </li>
             <?php
             endforeach;
