d523737965127008d5a06366712543cc554d1835
Myrrdin <stephan.jeanba@gmail.com>
2022-08-11 17:26:51 +0200

    [🚧 WIP]: ajouts de la Seed et des constances d'acces à la bdd

--- config/param.ini.php
+++ config/param.ini.php
@@ -1,9 +1,18 @@
 <?php
 
-// FORMULAIRE
-
+// SMTP server configuration
 define('SMTP_HOST', 'mail.jbscreative.dev');
 define('SMTP_USER', 'openclassroom@jbscreative.dev');
 define('SMTP_PASS', 'fakepassword1');
 
+// DB server configuration
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'blog');
+define('DB', 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8');
+// Security configuration
+define("CRSF_TOKEN_SECRET", "openclassroom");
+
+// ................
 include_once "vendor/autoload.php";
